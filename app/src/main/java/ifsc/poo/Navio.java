45d0fb0cd246f24a85915fbc27f3cc7ca436b776
FelipeABruschi <felipeaugbr@outlook.com>
2025-04-21 22:58:24 -0300

    comecando ex 6

---
+++ app/src/main/java/ifsc/poo/Navio.java
@@ -0,0 +1,12 @@
+package ifsc.poo;
+
+public class Navio {
+    private int tamanho;
+    private int linha;
+    private int coluna;
+    private int[][] posicao;
+    private char orientacao;
+    private char letra;
+    private int[][] posicao_atingida;
+    private boolean afundado;
+}
